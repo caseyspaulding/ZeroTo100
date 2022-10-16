e08d78ce022d5c7ee41177b975b31f13cd87b163
Casey Spaulding <casey.spaulding@me.com>
2022-10-16 19:29:05 -0400

    update js

--- js/site.js
+++ js/site.js
@@ -1,4 +1,66 @@
-//Call Hello World
-function helloWorld(){
-    alert("hello World");
+//get the values from the Page
+//starts or controller function
+function getValues() {
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    
+
+    //We NEED to validate our input
+    //parse into integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+        //we need to call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+        //we need to call displayNumbers
+        displayNumbers(numbers);
+    } else{
+        alert("You must enter integers");
+    }
+
+}
+
+//generate numbers from startvalue to the endValue
+//logic functions(s)
+function generateNumbers(sValue, eValue){
+
+    let numbers = [];
+
+    //get all numbers from start to end
+    for (let index = sValue; index <= eValue; index++){
+
+        //this will execute in a loop until index = eValue
+        numbers.push(index);
+    }
+    return numbers;
+
+}
+
+//display the numbers and marrk even numbers in bold
+//display or view functions
+function displayNumbers(numbers){
+
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+        
+        let className = "even";
+
+        let number = numbers[index];
+
+        if(number % 2 == 0){
+            className = "even";
+
+        }
+        else{
+            className = "odd";
+
+        }
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows
+
 }
